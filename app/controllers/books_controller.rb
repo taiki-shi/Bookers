16597e429813345163e3a8265c94a73d84176e59
taiki-shi <love.hurricane.exist@gmail.com>
2020-07-29 05:05:46 +0900

    update 2nd

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,42 @@
 class BooksController < ApplicationController
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
-  def new
+  def top
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(book)
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+  def update
+  	book = Book.find(params[:id])
+  	bool.update(book_params)
+  	redirect_to book_path(book)
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
 end
